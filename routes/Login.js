cb3ef97f52ffe17daac4dfa8b73b3e335396a578
Sai Sahith <saisahith.goud2021@vitstudent.ac.in>
2023-01-13 17:30:40 +0530

    changes in responses

--- routes/Login.js
+++ routes/Login.js
@@ -2,22 +2,22 @@ const express = require('express');
 const router = express.Router();
 const Authentication = require('../models/Authentication');
 
-router.get('/', async (req, res) => {
+router.post('/', async (req, res) => {
     try 
     {
         const user = await Authentication.findOne({ email:req.query.email });
         if (user.password === req.query.password) 
         {
-            res.status(200).send('Login Successful'); //Login successful
+            res.status(200).json({'status':'Login Successful'}); //Login successful
         } 
         else 
         {
-            res.status(400).send('Invalid Credentials'); //Incorrect Password
+            res.status(400).json({'status':'Incorrect Password'}); //Incorrect Password
         }
     } 
     catch (err) 
     {
-        res.status(401).send('User not Found'); //User not found
+        res.status(401).send({'status':'User not found'}); //User not found
     }
 });
 
