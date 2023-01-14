6d1b3b816704ad446ac847caeb02dcca5aef40a9
Sai Sahith <saisahith.goud2021@vitstudent.ac.in>
2023-01-14 10:10:02 +0530

    changed response of Rent

--- routes/Rent.js
+++ routes/Rent.js
@@ -17,7 +17,7 @@ router.post('/', async (req, res) => {
         remarks:'-'
     });
     newhistory.save();
-    res.status(200).json(await History.find());
+    res.status(200).json({'status':'History created'});
 });
 
 module.exports = router;
